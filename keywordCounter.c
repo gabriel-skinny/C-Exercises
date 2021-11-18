ae16e09704e3ae2241a74eec48cc225fe0dc518f
gabriel-skinny <gabrielcatoni12@gmail.com>
2021-11-18 16:01:58 -0300

    Changing the keywordcounter logic to support struct pointers instead of an array of structers

--- keywordCounter.c
+++ keywordCounter.c
@@ -14,74 +14,70 @@ struct key {
   "unsigned", 0,
   "void", 0,
   "volatile", 0,
-  "while", 0
+  "while", 0,
 };
 
 #define MAXWORD 100
 #define NKEYS (sizeof keytab / sizeof(keytab[0]))
 
 int getword(char *, int);
-int binarySearch(char *, struct key *, int);
+struct key *binarySearch(char *, struct key *, int);
 
 int main() {
   int n;
   char word[MAXWORD];
+  struct key *p;
 
   while ((getword(word, MAXWORD)) != EOF)
     if (isalpha(word[0])) 
-      if ((n = binarySearch(word, keytab, NKEYS)) >= 0){
-        keytab[n].count++;
-        printf("\nCount: %d Word: %s\n\n", keytab[n].count, keytab[n].word);
+      if ((p = binarySearch(word, keytab, NKEYS)) >= 0){
+        p -> count++;
+        printf("\nCount: %d Word: %s\n\n", p -> count, p -> word);
       }
 
   return 0;
 }
 
-int binarySearch(char *word, struct key tab[], int n) {
+struct key *binarySearch(char *word, struct key *tab, int n) {
   int cond;
-  int low, high, mid;
+  struct key *low, *high, *mid;
 
-  low = 0;
-  high = n - 1;
-  while(low <=high) {
-      mid = (low + high) / 2;
-      if ((cond = strcmp(word, tab[mid].word)) < 0)
-        high = mid - 1; 
+  low = &tab[0];
+  high = &tab[n];
+
+  while(low <= high) {
+      mid = low + (high - low) / 2;
+      if ((cond = strcmp(word, mid -> word)) < 0)
+        high = mid; 
       else if (cond > 0)
         low = mid + 1;
       else 
         return mid;
   }
 
-  return -1;
+  return NULL;
 }
 
 int getword(char *word, int lim) {
   int c, getchM(void);
-  void ungetchM(int);
-  char *w = word;
+	void ungetchM(int);
+	char *w = word;
 
-  while (isspace(c = getchM()))
-    ;
-
-  if (c != EOF)
-    *w++ = c;
-  
-  if (!isalpha(c)) {
-    *w = '\0';
-    return c;
-  }
-
-  for (; --lim > 0; w++) {
-    if (!isalnum(*w = getchM())) {
-        ungetchM(*w);
-        break;
-    }
-  }
-
-  *w = '\0';
-
-  return word[0];
+	while (isspace(c = getchM()))
+		;
+	if (c != EOF)
+		*w++ = c;
+	if (!isalpha(c)) {
+		*w = '\0';
+		return c;
+	}
+	for ( ; --lim > 0; w++)
+		if (!isalnum(*w = getchM())) {
+			ungetchM(*w);
+			break;
+		}
+	*w = '\0';
+	return word[0];
 }
 
 char buffer[MAXWORD];
