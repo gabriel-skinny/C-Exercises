9d4475ff5eb076fed96d9cf9c7c8c46fa41bde3a
gabriel-skinny <gabrielcatoni12@gmail.com>
2021-11-21 20:47:54 -0300

    Refatorin cat function to handle errors

--- Chapter7:IO/cat.c
+++ Chapter7:IO/cat.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   FILE *fp;
   void filecopy(FILE *, FILE *);
+  char *prog = argv[0];
 
   if (argc == 1)
     filecopy(stdin, stdout);
   else 
     while(--argc > 0)
       if ((fp = fopen(*++argv, "r")) == NULL) {
-        printf("cat: Cant open %s\n", *argv);
-        return 1;
+        fprintf(stderr ,"%s: Cant open %s\n",prog, *argv);
+        exit(1);
       }
       else {
         filecopy(fp, stdout);
         fclose(fp);
       }
+    
+    if (ferror(stdout)) {
+      fprintf(stderr, "%s: error wirting stdout\n", prog);
+      exit(2);
+    }
 
-  return 0;
+  exit(0);
 }
 
 void filecopy(FILE *inputFile, FILE *outputFile) {
