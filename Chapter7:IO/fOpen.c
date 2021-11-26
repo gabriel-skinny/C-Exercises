de592ec7c6ddcec633fa1922a98c5937fc0d317c
gabriel-skinny <gabrielcatoni12@gmail.com>
2021-11-26 11:11:41 -0300

    Adding flushbuff to add char to the buffer of a file

--- Chapter7:IO/fOpen.c
+++ Chapter7:IO/fOpen.c
@@ -11,16 +11,6 @@ typedef struct _iobuf {
   int fd;
 } FILE;
 
-extern FILE _iob[OPEN_MAX] = {
-  { 0, (char *) 0, (char *) 0, _READ, 0},
-  { 0, (char *) 0, (char *) 0, _WRITE, 0},
-  { 0, (char *) 0, (char *) 0, _WRITE | _UNBUF, 2 },
-};
-
-#define stdin (&_iob[0])
-#define stdout (&_iob[1])
-#define stdout (&_iob[3])
-
 enum _flags {
   _READ = 01,
   _WRITE = 02,
@@ -29,6 +19,16 @@ enum _flags {
   _ERR = 020
 };
 
+FILE _iob[OPEN_MAX] = {
+  { 0, (char *) 0, (char *) 0, _READ, 0},
+  { 0, (char *) 0, (char *) 0, _WRITE, 0},
+  { 0, (char *) 0, (char *) 0, _WRITE | _UNBUF, 2 },
+};
+
+#define stdin (&_iob[0])
+#define stdout (&_iob[1])
+#define stderr (&_iob[2])
+
 int _fillbuff(FILE *);
 int _flushbuff(int, FILE *);
 
@@ -36,12 +36,14 @@ int _flushbuff(int, FILE *);
 #define ferror(p) ((p) -> flag & _ERR) != 0)
 #define fileno(p) ((p) -> fd)
 
-#define getc(p) (--(p)->charsleft >= 0 ? (unsigned char) *(p) -> nextCharPos++ : _fillbuff(p))
+#define getc(p) (--(p)->charsleft >= 0 ? (unsigned char) *(p) -> nextCharPos++ : _fillbuf(p))
 #define putc(x, p) (--(p)->charsleft >= 0 ? *(p) -> nextCharPos++ = (x) : _flushbuf((x), p))
 
 
 #include <fcntl.h>
 #include "syscall.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 #define PERMS 0666
 
@@ -106,4 +108,58 @@ int _fillbuf(FILE *fp){
     }
 
     return (unsigned char) *fp -> nextCharPos++;
+}
+
+int _flushbuf(int c, FILE *fp) {
+  int bufsize, char_written;
+  unsigned char uc = c;
+  
+  if ((fp -> flag) & (_WRITE|_EOF|_ERR) != _WRITE)
+    return EOF;
+  
+  if (fp -> baseBuff == NULL && ((fp -> flag & _UNBUF) == 0)){
+     if ((fp -> baseBuff = malloc(BUFSIZ)) == NULL)
+      fp -> flag |= _UNBUF;
+    else {
+      fp -> nextCharPos = fp -> baseBuff;
+      fp -> charsleft = BUFSIZ - 1;
+    }
+  }
+
+  if (fp -> flag & _UNBUF) {
+    fp -> nextCharPos = fp -> baseBuff = NULL;
+    fp -> charsleft = 0;
+    if (c == EOF)
+      return EOF;
+
+    char_written = write(fp -> fd, &uc, 1);
+    bufsize = 1;
+  } 
+  else {
+    bufsize = (int)(fp -> nextCharPos - fp -> baseBuff);
+    char_written = write(fp -> fd, fp -> baseBuff, bufsize);
+    fp -> nextCharPos = fp -> baseBuff;
+    fp -> charsleft = BUFSIZ - 1;
+  }
+ 
+  if (char_written == bufsize)
+    return c;
+  else{
+    fp -> flag |= _ERR;
+    return EOF;
+  }
+  
+}
+
+
+int main() {
+
+  char alo;
+
+  alo = getc(stdin);
+  putc(alo, stdout);
+
+  
+
+  return 0;
 }
